18bde81bfc9fac749f7fd0a9c0a2700a50ffe816
bremp <byadao@gmail.com>
2020-10-12 19:03:37 -1000

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -2,16 +2,22 @@ package com.h2;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
 
-    public static int doubleTheNumber(int number) {
-        return -1;
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+  public static int doubleTheNumber(int number) {
+    return 2 * number;
+  }
+
+  private static int add(int[] numbers) {
+    int sum = 0;
+    for (int number : numbers) {
+      sum += number;
     }
+    return sum;
+  }
 }
